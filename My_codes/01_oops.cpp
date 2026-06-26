ee6901b326e048909818d55de60672ea7a2d9472
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2026-06-26 19:31:52 +0530

    Refactor Car class and add car types
    Refactor Car class to improve structure and add ElectricCar and ManualCar implementations.

--- My_codes/01_oops.cpp
+++ My_codes/01_oops.cpp
@@ -3,91 +3,163 @@ using namespace std;
 
 // Abstraction hides unnecessary details from the client.
 
-class Car {
-public:
-	virtual void startEngine() = 0;
-	virtual void shiftGear(int gear) = 0;
-	virtual void accelerate() = 0;
-	virtual void brake() = 0;
-	virtual void stopEngine() = 0;
-	virtual ~Car() {};
-};
+// Encapsulation says 2 things:
+// 1. An Object's Characteristics and its behaviour are encapsulated together
+// within that Object.
+// 2. All the characteristics or behaviours are not for everyone to access.
+// Object should provide data security.
 
-class SportsCar : public Car {
-private:
+// Dynamic polymorphism -> function overriding
+// we can have a virtual function in our abstract class Car named accelerate
+// but their children i.e. manual car and electric car will define them differently
+
+class Car {
+protected:
 	string brand;
 	string model;
 	bool isEngineOn;
 	int currentSpeed;
-	int currentGear;
-	string tyre;
 
 public:
-	SportsCar(string brand, string model) {
+	Car(string brand, string model) {
 		this->brand = brand;
 		this->model = model;
-		isEngineOn = false;
-		currentSpeed = 0;
-		currentGear = 0;
-		tyre = "CEAT";
- 	}
-
- 	string getTyre() {
- 		return this->tyre;
- 	}
-
- 	void setTyre(string tyre) {
- 		// validations : control using getters and setters
- 		this->tyre = tyre;
- 	}
-
-	void startEngine() {
-		isEngineOn = true;
-		cout << brand << " " << model << " : Engine starts with a roar!" << endl;
+		this->isEngineOn = false;
+		this->currentSpeed = 0;
 	}
 
+	// common methods for all cars
+	void startEngine() {
+		isEngineOn = true;
+		cout << brand << " " << model << " : Engine started!" << endl;
+	}
+
+	void stopEngine() {
+		isEngineOn = false;
+		currentSpeed = 0;
+		cout << brand << " " << model << " : Engine turned off!" << endl;
+	}
+
+	virtual void accelerate() = 0; // abstract method for dynamic polymorphism
+	virtual void accelerate(int speed) = 0; // abstract method for dynamic polymorphism
+	virtual void brake() = 0; // abstract method for dynamic polymorphism
+	virtual ~Car() {}; // virtual destructor
+};
+
+class ManualCar : public Car {
+private:
+	int currentGear;
+
+public:
+	ManualCar(string brand, string model) : Car(brand, model) {
+		this->currentGear = 0;
+	}
+
+	// specialized method for manual car
 	void shiftGear(int gear) {
-        if (!isEngineOn) {
-            cout << brand << " " << model << " : Engine is off! Cannot Shift Gear." << endl;
-            return;
-        }
         currentGear = gear;
         cout << brand << " " << model << " : Shifted to gear " << currentGear << endl;
-    }
+	}
 
-    void accelerate() {
-        if (!isEngineOn) {
+	// overriding accelerate -> dynamic polymorphism
+	void accelerate() {
+		if(!isEngineOn) {
             cout << brand << " " << model << " : Engine is off! Cannot accelerate." << endl;
             return;
         }
         currentSpeed += 20;
         cout << brand << " " << model << " : Accelerating to " << currentSpeed << " km/h" << endl;
-    }
+	}
 
-    void brake() {
+	// overloading and overloading accelerate
+	void accelerate(int speed) {
+		if(!isEngineOn) {
+            cout << brand << " " << model << " : Engine is off! Cannot accelerate." << endl;
+            return;
+        }
+        currentSpeed += speed;
+        cout << brand << " " << model << " : Accelerating to " << currentSpeed << " km/h" << endl;
+	}
+
+	// overriding brake
+	void brake() {
         currentSpeed -= 20;
-        if (currentSpeed < 0) currentSpeed = 0;
+        if(currentSpeed < 0) currentSpeed = 0;
         cout << brand << " " << model << " : Braking! Speed is now " << currentSpeed << " km/h" << endl;
     }
+};
 
-    void stopEngine() {
-        isEngineOn = false;
-        currentGear = 0;
-        currentSpeed = 0;
-        cout << brand << " " << model << " : Engine turned off." << endl;
+class ElectricCar : public Car {
+private:
+	int batteryLevel;
+
+public:
+	ElectricCar(string brand, string model) : Car(brand, model) {
+		this->batteryLevel = 100;
+	}
+
+	// specialized method for electric car
+	void chargeBattery() {
+        batteryLevel = 100;
+        cout << brand << " " << model << " : Battery fully charged!" << endl;
+	}
+
+	// overriding accelerate -> dynamic ploymorphism
+	void accelerate() {
+		if(!isEngineOn) {
+            cout << brand << " " << model << " : Engine is off! Cannot accelerate." << endl;
+            return;
+        }
+        if(batteryLevel <= 0) {
+        	cout << brand << " " << model << " : Battery dead! Cannot accelerate." << endl;
+        }
+        batteryLevel -= 10;
+        currentSpeed += 15;
+        cout << brand << " " << model << " : Accelerating to " << currentSpeed << " km/h. Battery at " << batteryLevel << "%." << endl;
+	}
+
+	// overloading and overloading accelerate
+	void accelerate(int speed) {
+		if(!isEngineOn) {
+            cout << brand << " " << model << " : Engine is off! Cannot accelerate." << endl;
+            return;
+        }
+        if(batteryLevel <= 0) {
+        	cout << brand << " " << model << " : Battery dead! Cannot accelerate." << endl;
+        }
+        batteryLevel -= 10 + speed;
+        currentSpeed += speed;
+        cout << brand << " " << model << " : Accelerating to " << currentSpeed << " km/h. Battery at " << batteryLevel << "%." << endl;
+	}
+
+	// overriding brake
+	void brake() {
+        currentSpeed -= 15;
+        if(currentSpeed < 0) currentSpeed = 0;
+        cout << brand << " " << model << " : Braking! Speed is now " << currentSpeed << " km/h" << endl;
     }
 };
 
 int main() {
-	Car* myCar = new SportsCar("Ford", "Mustang");
+	Car* myManualCar = new ManualCar("Suzuki", "WagonR");
+    myManualCar->startEngine();
+    myManualCar->accelerate();
+    myManualCar->accelerate(); // function overloading
+    myManualCar->accelerate(50); // function overloading
+    myManualCar->brake();
+    myManualCar->stopEngine();
 
-	myCar->startEngine();
-    myCar->shiftGear(1);
-    myCar->accelerate();
-    myCar->shiftGear(2);
-    myCar->accelerate();
-    myCar->brake();
-    myCar->stopEngine();
+    cout << "----------------------" << endl;
 
-    delete myCar;
+    Car* myElectricCar = new ElectricCar("Tesla", "Model S");
+    myElectricCar->startEngine();
+    myElectricCar->accelerate();
+    myElectricCar->accelerate(); // function overloading
+    myElectricCar->accelerate(40); // function overloading
+    myElectricCar->brake();
+    myElectricCar->stopEngine();
+
+    // Cleanup
+    delete myManualCar;
+    delete myElectricCar;
 }
